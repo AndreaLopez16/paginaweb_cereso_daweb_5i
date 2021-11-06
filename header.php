d468e2ab84032dafb6d3191e5002a1f9f2ce6a62
Andrea Ivonne Lopez Iturbe <andreaivlopez@hotmail.com>
2021-11-05 22:12:18 -0600

    subiendo archivos

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>Cereso</title>
+
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<img src="img/cereso.jpg" alt="Ceresos">
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="guardias.php">Guardias</a></li>
+					<li><a href="reo.php">Reo</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</body>
